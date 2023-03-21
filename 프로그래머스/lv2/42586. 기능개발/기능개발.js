b7a03423ff5b71deefecf5c7b904b3176bf86b39
sarang_daddy <109648042+sarangdaddy@users.noreply.github.com>
2023-03-21 23:43:17 +0900

    [level 2] Title: 기능개발, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -3,11 +3,11 @@ function solution(progresses, speeds) {
 
   const endCountArray = [];
 
-  for (let i = 0; i < progresses.length; i++) {
-    const endCount = Math.ceil((100 - progresses[i]) / speeds[i]);
+  progresses.forEach((workload, i) => {
+    const endCount = Math.ceil((100 - workload) / speeds[i]);
     endCountArray.push(endCount);
-  }
-
+  });
+    
   while (endCountArray.length > 0) {
     const minusCount = endCountArray[0];
     for (let i = 0; i < endCountArray.length; i++) {
