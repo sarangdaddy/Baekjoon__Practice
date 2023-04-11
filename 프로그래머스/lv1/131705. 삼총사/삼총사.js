f4a72ca4f7f09797cfd9468c7bfa151948cad495
sarang_daddy <109648042+sarangdaddy@users.noreply.github.com>
2023-04-12 00:09:15 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,24 +1,15 @@
 function solution(number) {
   let answer = 0;
-  let pointOne = 0;
-  let pointTwo = 1;
-  let pointThree = 2;
 
-  for (let i = pointOne; i < number.length - 2; i++) {
-    for (let j = pointTwo; j < number.length - 1; j++) {
-      for (let k = pointThree; k < number.length; k++) {
+  for (let i = 0; i < number.length - 2; i++) {
+    for (let j = i + 1; j < number.length - 1; j++) {
+      for (let k = j + 1; k < number.length; k++) {
         let sum = number[i] + number[j] + number[k];
         if (sum === 0) {
           answer++;
         }
-        pointThree++;
       }
-      pointTwo++;
-      pointThree = pointTwo + 1;
     }
-    pointOne++;
-    pointTwo = pointOne + 1;
-    pointThree = pointTwo + 1;
   }
 
   return answer;
